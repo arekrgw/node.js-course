6e34fe33d06683f67d564558cdb072627eaf38e9
ArekRGW <arkadiusz.pawlak.web@gmail.com>
2019-08-16 20:18:41 +0200

    Finished partials section

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,52 @@
+const path = require("path");
 const express = require("express");
+const hbs = require("hbs")
 
+hbs.par
 
 const app = express();
 
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
+
+//Handlebars setup
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(path.join(__dirname, "../public")))
+
+
 app.get("", (req, res) => {
-    res.send("sdasdasdsa")
-});
+    res.render("index", {
+        title: "Weather app",
+        name: "Arek Pawlak"
+    })
+})
 
 app.get("/about", (req, res) => {
-    res.send("About page")
-});
+    res.render("about", {
+        title: "About me",
+        name: "Arek Pawlak"
+    })
+})
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        message: "This is a help page",
+        title: "Help",
+        name: "Arek Pawlak"
+    })
+})
 
 app.get("/weather", (req, res) => {
-    res.send("Weather page")
+    res.send({
+        place: "Rogów",
+        lon: 21,
+        lat: 53,
+        temperature: 17
+    })
 });
 
 
